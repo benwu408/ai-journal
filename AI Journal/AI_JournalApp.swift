33daba54c0e06ba8510777cb2f6e4d41ca7c4ea8
Ben Wu <benwu408@gmail.com>
2025-06-13 09:29:50 -0500

    Initial Commit

---
+++ AI Journal/AI_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_JournalApp.swift
+//  AI Journal
+//
+//  Created by Ben Wu on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AI_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI JournalUITests/AI_JournalUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AI_JournalUITestsLaunchTests.swift
+//  AI JournalUITests
+//
+//  Created by Ben Wu on 6/13/25.
+//
+
+import XCTest
+
+final class AI_JournalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
